c57ebd92cb541164bcb1cc0157543f2ebda4ff44
levi <levitimothy@mail.weber.edu>
2024-07-23 14:21:00 -0600

    Initial Commit

---
+++ GolfScores/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GolfScores
+//
+//  Created by Joanne Timothy on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GolfScores/GolfScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfScoresApp.swift
+//  GolfScores
+//
+//  Created by Joanne Timothy on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GolfScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
